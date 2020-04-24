41829df6112854601a844a0aa7aa409cc9be9e62
Paulo Langer <paulo.langer@xing.com>
2020-04-24 20:37:14 +0100

    creating & running benchmarking tests

---
+++ benchmarking/benchmarking_test.go
@@ -0,0 +1,36 @@
+// Benchmarking
+// $ go test -bench (running)
+// $ go test -bench -benchtime 10s (targeting specified time)
+
+package benchmarking
+
+import (
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"testing"
+)
+
+func BenchmarkSHA1(b *testing.B) {
+	data := []byte("Marry had a little lamb")
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		sha1.Sum(data)
+	}
+}
+
+func BenchmarkSHA256(b *testing.B) {
+	data := []byte("Marry had a little lamb")
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		sha256.Sum256(data)
+	}
+}
+
+func BenchmarkSHA512(b *testing.B) {
+	data := []byte("Marry had a little lamb")
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		sha512.Sum512(data)
+	}
+}
